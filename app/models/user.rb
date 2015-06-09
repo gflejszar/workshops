7339c88ca40ab2048183f97cb3fa7f9c5ed8c508
Grzegorz Flejszar <flejszar@gmail.com>
2015-06-09 19:31:09 +0000

    Add firstname, lastname and admin fields to user. Add validations to model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,10 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-
+  
+  validates :firstname, presence: true
+  validates :lastname, presence: true
+  
   has_many :reviews
   has_many :products
 end
