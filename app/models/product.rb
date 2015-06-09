6dd9f4164085f37a40909a2d5c2688a05c9b9c6d
Grzegorz Flejszar <flejszar@gmail.com>
2015-06-09 19:48:16 +0000

    Add validations and average_rating method

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,12 @@ class Product < ActiveRecord::Base
   belongs_to :category
   belongs_to :user
   has_many :reviews
+  
+  validates :title, presence: true
+  validates :price, presence: true, format: { :with => /\A\d+(?:\.\d{0,2})?\z/ }
+  validates :description, presence: true
+  
+  def average_rating
+     self.reviews.average(:rating)
+  end
 end
