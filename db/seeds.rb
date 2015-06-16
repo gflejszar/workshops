e8d4d1d1a4397cb543e5674663e9b39a4e4b7931
Grzegorz Flejszar <flejszar@gmail.com>
2015-06-16 19:27:37 +0000

    Add user to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create(email: "bob@example.com", firstname: "bob", lastname: "admin", password: "topsecret", password_confirmation: "topsecret", admin: true )
+User.create(email: "john@example.com", firstname: "john", lastname: "user", password: "topsecret", password_confirmation: "topsecret" )
+User.create(email: "ed@example.com", firstname: "ed", lastname: "user", password: "topsecret", password_confirmation: "topsecret" )
+User.create(email: "luc@example.com", firstname: "luc", lastname: "user", password: "topsecret", password_confirmation: "topsecret" )
+User.create(email: "gus@example.com", firstname: "gus", lastname: "user", password: "topsecret", password_confirmation: "topsecret" )
