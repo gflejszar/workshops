27d34bf67eb65c4be42a58cffdc8ef39c49aa908
Grzegorz Flejszar <flejszar@gmail.com>
2015-06-18 18:40:38 +0000

    Updates seed file.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,26 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+lorem_ipsum = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
 
-User.create(email: "bob@example.com", firstname: "bob", lastname: "admin", password: "topsecret", password_confirmation: "topsecret", admin: true )
-User.create(email: "john@example.com", firstname: "john", lastname: "user", password: "topsecret", password_confirmation: "topsecret" )
-User.create(email: "ed@example.com", firstname: "ed", lastname: "user", password: "topsecret", password_confirmation: "topsecret" )
-User.create(email: "luc@example.com", firstname: "luc", lastname: "user", password: "topsecret", password_confirmation: "topsecret" )
-User.create(email: "gus@example.com", firstname: "gus", lastname: "user", password: "topsecret", password_confirmation: "topsecret" )
+admins = ["bob"]
+users = ["john", "ed", "luc", "gus"]
+
+admins.each do |admin|
+  User.create(email: "#{admin}@example.com", firstname: "#{admin}", lastname: "admin", password: "topsecret", password_confirmation: "topsecret", admin: true)
+end
+
+users.each do |user|
+  User.create(email: "#{user}@example.com", firstname: "#{user}", lastname: "user", password: "topsecret", password_confirmation: "topsecret")
+end
+
+for i in 1..5
+  category = Category.create(name: "Category#{i}")
+  for j in 1..5
+    product = Product.create(title: "Product#{j}", description: lorem_ipsum, price: 12.34, category: category)
+    User.all.each do |user|
+      Review.create(content: lorem_ipsum, rating: j, product: product, user: user)
+    end
+  end
+end
+
